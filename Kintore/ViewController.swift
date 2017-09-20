e91659dbdd38958ef8d5b909cc3e8265146d637d
Saki Matsumoto <kinoko@ueo.co.jp>
2017-09-20 11:37:51 +0900

    Create repository

---
+++ Kintore/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Kintore
+//
+//  Created by superkinoko on 2017/09/20.
+//  Copyright © 2017年 kinokodata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func startButtonAction(_ sender: Any) {
+    }
+
+}
+
